4560fd46c65144994b88d70c41f9c8fd89f11a7d
udayveershaktawat <udayveershaktawat@gmail.com>
2026-03-27 23:57:45 +0530

    connceted

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import dbConnect from "./config/dbconnect.js";
 
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000
+
+
+dbConnect();
 
 app.listen(PORT, () => {
   console.log(`server started at Port number ${PORT} Successfully`);
