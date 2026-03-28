9f0635fe76e4d304d4207d60864398cda546464f
udayveershaktawat <udayveershaktawat@gmail.com>
2026-03-28 11:56:19 +0530

    frontend creating

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h1 class=" font-bold underline">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default App
