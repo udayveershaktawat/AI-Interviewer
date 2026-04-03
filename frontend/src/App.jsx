019bc7335b24c85961bd3452c67bab38550f1771
udayveershaktawat <udayveershaktawat@gmail.com>
2026-04-03 23:57:27 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,33 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Auth from './pages/Auth'
-import { useEffect } from 'react'
-import axios from "axios"
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import { useEffect } from "react";
+import axios from "axios";
 
-
-export const serverUrl = "http://localhost:4000"
+export const serverUrl = "http://localhost:4000";
 
 const App = () => {
-
-  useEffect(()=>{
+  useEffect(() => {
     const getUser = async () => {
       try {
-        const result = await axios.get(serverUrl + "/api/user/current-user", {withCredentials:true})
-        console.log(result.data)
-        
+        const result = await axios.get(serverUrl + "/api/user/current-user", {
+          withCredentials: true,
+        });
+        console.log(result.data);
       } catch (error) {
-        console.log(error)
-        
+        console.log(error);
       }
-      
-    }
-    getUser()
-  },[])
-
+    };
+    getUser();
+  }, []);
 
   return (
-  <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path="/auth" element={<Auth/>} />
-  </Routes>
-  
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
