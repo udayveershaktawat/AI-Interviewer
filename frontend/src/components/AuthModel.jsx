07c1d3d7f4dbdaad15d566a7152256a0046fc4f8
udayveershaktawat <udayveershaktawat@gmail.com>
2026-04-11 23:55:47 +0530

    updated

--- frontend/src/components/AuthModel.jsx
+++ frontend/src/components/AuthModel.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { FaTimes } from "react-icons/fa";
 
-const AuthModel = () => {
+const AuthModel = ({ onClose }) => {
+  const { userData } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (userData) {
+      onClose();
+    }
+  }, [userData, onClose]);
+
   return (
-    <div>
-      <div>
-        <div>3:15
-
-
-          
-        </div>
+    <div className="fixed insert-0 z-[999] flex items-center justify-center bg-black/10 backdrop:backdrop-blur-sm px-4">
+      <div className="relative w-full max-w-md">
+        <button className="absolute top-8 right-5 text-gray-800 hover:text-black text-xl ">
+          <FaTimes size={18} />
+        </button>
       </div>
-
-
-      
     </div>
-  )
-}
+  );
+};
 
-export default AuthModel
+export default AuthModel;
