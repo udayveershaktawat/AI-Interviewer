cfa35d6113b9478ce6a36ccda74ce6c9802d571c
udayveershaktawat <udayveershaktawat@gmail.com>
2026-04-12 00:00:04 +0530

    auth update

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -10,7 +10,7 @@ import axios from "axios"
 import { useDispatch } from 'react-redux';
 import { setUserData } from '../redux/userSlice';
 
-const Auth = () => {
+const Auth = ({isModel = false}) => {
 
     const dispatch = useDispatch()
 
