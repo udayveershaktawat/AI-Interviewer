aac1a8c663896487aaf7d7538da1bca40757b050
udayveershaktawat <udayveershaktawat@gmail.com>
2026-04-26 23:33:45 +0530

    restart workinhg

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,31 @@
 import express from "express";
 import dbConnect from "./config/dbconnect.js";
-import cors from "cors"
+import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import authRouter from "./routes/auth.route.js"
-import morgan from "morgan"
+import authRouter from "./routes/auth.route.js";
+import morgan from "morgan";
 import userRouter from "./routes/user.route.js";
-
-
 
 const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+app.use(morgan("dev"));
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true
-}))
-app.use(morgan("dev"))
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
-
-
-
-app.use(express.urlencoded({extended:true}))
-
-
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   console.log(`server started at Port number ${PORT} Successfully`);
