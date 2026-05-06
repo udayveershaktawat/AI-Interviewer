78957632c57cdf859cb673c4c0fe2d81a40c40c9
udayveershaktawat <udayveershaktawat@gmail.com>
2026-05-06 14:09:14 +0530

    ai step completed

---
+++ backend/services/openRouter.services.js
@@ -0,0 +1,11 @@
+export const askAi  = async (messages) => {
+    try {
+        if(!message || !Array.isArray(message) || message.length ===0){
+            throw new Error("message error is empty")
+        }
+        
+    } catch (error) {
+        
+    }
+    
+}
