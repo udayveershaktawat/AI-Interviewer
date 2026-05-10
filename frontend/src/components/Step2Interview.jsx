463ec8658655a00f0fb1a964d6e0a2b7966f3a45
udayveershaktawat <udayveershaktawat@gmail.com>
2026-05-10 14:59:12 +0530

    timer created

--- frontend/src/components/Step2Interview.jsx
+++ frontend/src/components/Step2Interview.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
+import React from "react";
+import maleVideo from "../assets/videos/male-ai.mp4"
+import femaleVideo from "../assets/videos/female-ai.mp4"
 
-const Step2Interview = ({interviewData, onFinish}) => {
+const Step2Interview = ({ interviewData, onFinish }) => {
+  const { interviewId, questions, userName } = interviewData;
   return (
-    <div>
-      wwe
-      
-    </div>
-  )
-}
+    <div className="min-h-screen bg-linear-to-br from-emerald-50 via-white to-teal-100 flex items-center justify-center p-4 sm:p-6">
+      <div className="w-full max-w-350 min-h-[80vh] bg-white rounded-3xl shadow-2xl border border-gray-200 flex flex-col lg:flex-row overflow-hidden">
+        {/* video section */}
+        <div className="w-full lg:w-[35%] bg-white flex flex-col items-center p-6 space-y-6 border-r border-gray-200">
 
-export default Step2Interview
+  <div className="w-full max-w-md rounded-2xl overflow-hidden shadow-xl">
+
+    <video
+      src={femaleVideo}
+      muted
+      playsInline
+      preload="auto"
+      className="w-full h-auto object-cover"
+    />
+
+  </div>
+  {/* timer Area */}
+
+<div className="w-full max-w-md bg-white border border-gray-200 rounded-2xl shadow-md p-6 space-y-5">
+
+  <div className="flex justify-between items-center">
+
+    <span className="text-sm text-gray-500">
+      Interview Status
+    </span>
+
+    <span className="text-sm font-semibold text-emerald-600">
+      AI Speaking
+    </span>
+
+  </div>
+
+  <div className="h-px bg-gray-200"></div>
+
+</div>
+
+</div>
+      </div>
+    </div>
+  );
+};
+
+export default Step2Interview;
