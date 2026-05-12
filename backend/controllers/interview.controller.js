b919c465750344197003f427fcf3136bcc3da69b
udayveershaktawat <udayveershaktawat@gmail.com>
2026-05-13 02:25:05 +0530

    get interview created - controller

--- backend/controllers/interview.controller.js
+++ backend/controllers/interview.controller.js
@@ -482,3 +482,58 @@ export const getMyInterviews = async (req,res) => {
   
 
 }
+
+
+export const getInterviewReport = async (req,res) => {
+  try {
+    const interview = await Interview.findById(req.params.id)
+
+    if(!interview){ 
+      return res.status(404).json({message:"Interview not found"})
+    }
+    const totalQuestions = interview.questions.length;
+
+
+let totalConfidence = 0;
+let totalCommunication = 0;
+let totalCorrectness = 0;
+
+interview.questions.forEach((q) => {
+  
+  totalConfidence += q.confidence || 0;
+  totalCommunication += q.communication || 0;
+  totalCorrectness += q.correctness || 0;
+});
+
+
+const avgConfidence = totalQuestions
+  ? totalConfidence / totalQuestions
+  : 0;
+
+const avgCommunication = totalQuestions
+  ? totalCommunication / totalQuestions
+  : 0;
+
+const avgCorrectness = totalQuestions
+  ? totalCorrectness / totalQuestions
+  : 0;
+
+
+  return res.json({
+    finalScore: interview.finalScore,
+  confidence: Number(avgConfidence.toFixed(1)),
+  communication: Number(avgCommunication.toFixed(1)),
+  correctness: Number(avgCorrectness.toFixed(1)),
+  questionWiseScore:interview.questions
+
+  })
+
+    
+  } catch (error) {
+     return res.status(500).json({
+      message:`failed to currrent user interview ${error}`
+    })
+    
+  }
+  
+}
