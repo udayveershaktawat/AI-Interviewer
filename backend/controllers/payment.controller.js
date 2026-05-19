f5cf030fc3f42c8404a9da252e70719b4b1ad19d
udayveershaktawat <udayveershaktawat@gmail.com>
2026-05-20 00:29:20 +0530

    payment verify controller

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -38,4 +38,36 @@ return res.json(order)
   }
 };
 
+export const verifyPayment = async (req, res) => {
+  try {
+    const {
+      razorpay_order_id,
+      razorpay_payment_id,
+      razorpay_signature,
+    } = req.body;
 
+    const body =
+      razorpay_order_id + "|" + razorpay_payment_id;
+
+    const expectedSignature = crypto
+      .createHmac(
+        "sha256",
+        process.env.RAZORPAY_KEY_SECRET
+      )
+      .update(body)
+      .digest("hex");
+
+    if (expectedSignature !== razorpay_signature) {
+      return res.status(400).json({
+        message: "Invalid payment signature",
+      });
+    }
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Payment verification failed",
+    });
+  }
+};
